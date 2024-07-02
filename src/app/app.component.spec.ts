3857a6ff411b5265d04352d15c73d7f7a854576b
cosati <cosati.alexandre@gmail.com>
2024-07-02 21:58:33 +0200

    [AppComponent] Test component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,72 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { UploadPictureComponent } from './upload-picture/upload-picture.component';
+import { LatLng, latLng } from 'leaflet';
+import { MapComponent } from './map/map.component';
+import { Picture } from './models/picture.model';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let uploadComponentSpy: jasmine.SpyObj<UploadPictureComponent>;
+  let mapComponentSpy: jasmine.SpyObj<MapComponent>;
+
+  const uploadedPicture: Picture = {
+    id: '2',
+    name: 'Krakow',
+    description: 'Wawel Castle',
+    path: 'wawel.jpg',
+    date: new Date('2023-07-20'),
+    latitude: 50.0647,
+    longitude: 19.9478,
+  };
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = new AppComponent;
+    fixture.detectChanges();
+
+    uploadComponentSpy = 
+      jasmine.createSpyObj('UploadPictureComponent', ['setCoordinates']);
+    component.uploadComponent = uploadComponentSpy as UploadPictureComponent;
+
+    mapComponentSpy = 
+      jasmine.createSpyObj('MapComponent', ['plotNewLocation']);
+    component.mapComponent = mapComponentSpy as MapComponent;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should call uploadComponent.setCoordinates on map click', () => {
+    const clickedCoordinates = latLng(50.056074, 19.951778);
+
+    component.onMapClick(clickedCoordinates);
+
+    expect(uploadComponentSpy.setCoordinates)
+      .toHaveBeenCalledOnceWith(clickedCoordinates);
+  });
+
+  it('should call mapComponent.plotNewLocation on add new picture', () => {
+    component.onAddPicture(uploadedPicture);
+
+    expect(mapComponentSpy.plotNewLocation)
+      .toHaveBeenCalledOnceWith(uploadedPicture);
+  });
+
+  it('should add uploaded picture to array.', () => {
+    component.onAddPicture(uploadedPicture);
+
+    expect(component.pictures).toContain(uploadedPicture);
   });
 
 });
