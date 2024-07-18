a4be790e333c38fca5ce5b6889befa544197fe6e
cosati <cosati@hotmail.com>
2024-07-18 19:38:49 +0200

    [TEST] Fix map component test

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapComponent } from './map.component';
+import { PicturesService } from '../services/pictures.service';
+import { of } from 'rxjs';
 
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
 
+  let picturesServiceSpy: jasmine.SpyObj<PicturesService>;
+
   beforeEach(async () => {
+    picturesServiceSpy = jasmine.createSpyObj('PicturesService', [
+      'getPictures',
+    ]);
+
+    picturesServiceSpy.getPictures.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
       imports: [MapComponent],
+      providers: [{ provide: PicturesService, useValue: picturesServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MapComponent);
